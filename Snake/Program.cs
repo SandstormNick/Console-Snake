4f7e81370477ec6595d6f99fce0602f12002c7d5
SandstormNick <gibbensnick@gmail.com>
2020-05-27 22:29:50 +0200

    Snake head is drawn and the state updates depending on key input

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Snake
 {
@@ -10,11 +11,33 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            //---------------------------------
+
+            ConsoleKeyInfo cs = new ConsoleKeyInfo();
+
+            //---------------------------------
+
             Field theField = new Field();
+            Game theGame = new Game();
+            Snake snake = new Snake(new ConcreteStateDown());
+            snake.Request();
             theField.SetField();
+            while (theGame.GetGameStatus() == true && cs.Key != ConsoleKey.Q)
+            {
+                while (Console.KeyAvailable == false)
+                {
+                    Thread.Sleep(300);
 
+                    snake.Request();
 
-            Console.ReadLine();
+                    snake.DrawSnake();
+
+                }
+                cs = Console.ReadKey(true);
+                snake.ReadKeyInput(cs);
+            }
+
+            
         }
     }
 
@@ -25,6 +48,20 @@ namespace Snake
     // - Field -> th parameters that the snake can roam in
     // - Game -> keeps track of the game status - points, if snake is still alive, etc, snake speed
     //
-    // Objective 1: Get the field displaying correctly - consider taking user input to determine the dimensions
-
+    //Set collision methods
+    //Create a fruit that generates randomly on the field
 }
+
+
+//==================================================================================================
+//Code to help get user input while the game loop is running - can delete at a later stage
+
+//ConsoleKeyInfo cs = new ConsoleKeyInfo();
+//do
+//{
+//    Console.WriteLine("\nPress a key to display; " + "press the 'P' key to quit.");
+//    while (Console.KeyAvailable == false) Thread.Sleep(200);
+//    cs = Console.ReadKey(true);
+//    Console.WriteLine("You pressed the '{0}' key.", cs.Key);
+//}
+//while (cs.Key != ConsoleKey.Q);
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -6,7 +6,212 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Snake
+    public class Snake
     {
+        private StateBase _state;
+        private string SnakeHead { get; set; }
+        private int XPosition { get; set; }
+        private int YPosition { get; set; }
+
+        private int PreviousYPosition { get; set; }
+        private int PreviousXPosition { get; set; }
+
+        public Snake(StateBase state)
+        {
+            SnakeHead = ">";
+            XPosition = 1;
+            YPosition = 1;
+            PreviousXPosition = 1;
+            PreviousYPosition = 1;
+
+            _state = state;
+        }
+
+        public StateBase State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public void Request()
+        {
+            _state.UpdatePreviousCoordinates(this);
+            _state.Handle(this);
+            _state.UpdateSnakeHead(this);
+        }
+
+        public void ReadKeyInput(ConsoleKeyInfo keyPressed)
+        {
+            switch (keyPressed.Key)
+            {
+                case ConsoleKey.W:
+                    _state = new ConcreteStateUp();
+                    break;
+                case ConsoleKey.S:
+                    _state = new ConcreteStateDown();
+                    break;
+                case ConsoleKey.D:
+                    _state = new ConcreteStateRight();
+                    break;
+                case ConsoleKey.A:
+                    _state = new ConcreteStateLeft();
+                    break;
+            }
+        }
+
+        public void DrawSnake()
+        {
+            Console.SetCursorPosition(GetPreviousYPosition(), GetPreviousXPosition());
+            Console.Write(" ");
+
+            Console.SetCursorPosition(GetYPosition(), GetXPosition());
+            Console.Write(GetSnakeHead());
+        }
+
+        public void SetSnakeHead(string value)
+        {
+            SnakeHead = value;
+        }
+
+        #region Get X and Y Coordinates
+        public int GetYPosition()
+        {
+            return YPosition;
+        }
+
+        public int GetXPosition()
+        {
+            return XPosition;
+        }
+
+        public int GetPreviousYPosition()
+        {
+            return PreviousYPosition;
+        }
+
+        public int GetPreviousXPosition()
+        {
+            return PreviousXPosition;
+        }
+        #endregion
+
+        public string GetSnakeHead()
+        {
+            return SnakeHead;
+        }
+
+        #region Move Coordinates
+        public void MoveDown()
+        {
+            XPosition++;
+        }
+
+        public void MoveUp()
+        {
+            XPosition--;
+        }
+
+        public void MoveRight()
+        {
+            YPosition++;
+        }
+
+        public void MoveLeft()
+        {
+            YPosition--;
+        }
+        #endregion
+
+        public void SetPreviousPositions(int currentX, int currentY)
+        {
+            PreviousXPosition = currentX;
+            PreviousYPosition = currentY;
+        }
     }
+
+    #region State Classes
+    public abstract class StateBase
+    {
+        public abstract void Handle(Snake snake);
+        public abstract void UpdateSnakeHead(Snake snake);
+        public abstract void UpdatePreviousCoordinates(Snake snake);
+    }
+
+    public class ConcreteStateDown : StateBase
+    {
+        public override void Handle(Snake snake)
+        {
+            snake.State = this;
+            snake.MoveDown();
+            
+        }
+
+        public override void UpdatePreviousCoordinates(Snake snake)
+        {
+            snake.SetPreviousPositions(snake.GetXPosition(), snake.GetYPosition());
+        }
+
+        public override void UpdateSnakeHead(Snake snake)
+        {
+            snake.SetSnakeHead("V");
+        }
+    }
+
+    public class ConcreteStateUp : StateBase
+    {
+        public override void Handle(Snake snake)
+        {
+            snake.State = this;
+            snake.MoveUp();
+        }
+
+        public override void UpdatePreviousCoordinates(Snake snake)
+        {
+            snake.SetPreviousPositions(snake.GetXPosition(), snake.GetYPosition());
+        }
+
+        public override void UpdateSnakeHead(Snake snake)
+        {
+            snake.SetSnakeHead("^");
+        }
+    }
+
+    public class ConcreteStateRight : StateBase
+    {
+        public override void Handle(Snake snake)
+        {
+            snake.State = this;
+            snake.MoveRight();
+        }
+
+        public override void UpdatePreviousCoordinates(Snake snake)
+        {
+            snake.SetPreviousPositions(snake.GetXPosition(), snake.GetYPosition());
+        }
+
+        public override void UpdateSnakeHead(Snake snake)
+        {
+            snake.SetSnakeHead(">");
+        }
+    }
+
+    public class ConcreteStateLeft : StateBase
+    {
+        public override void Handle(Snake snake)
+        {
+            snake.State = this;
+            snake.MoveLeft();
+        }
+
+        public override void UpdatePreviousCoordinates(Snake snake)
+        {
+            snake.SetPreviousPositions(snake.GetXPosition(), snake.GetYPosition());
+        }
+
+        public override void UpdateSnakeHead(Snake snake)
+        {
+            snake.SetSnakeHead("<");
+        }
+    }
+    #endregion
 }
